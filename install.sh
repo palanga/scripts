1ac6773526f8a8ba1ca2335f2664041fd3258c08
palanga <a.gonzalez.terres@gmail.com>
2018-06-06 02:18:12 -0300

    Change logging with a simple echo for a custom pretty colorful one

--- install.sh
+++ install.sh
@@ -1,59 +1,84 @@
+function log() {
+	option=$1
+	message=$2
+
+	GREEN='\033[0;32m'
+	YELLOW='\033[0;33m'
+	BLUE='\033[0;34m'
+	RED='\033[0;31m'
+	NC='\033[0m'
+
+	BASE=''
+	case $option in
+		-i )
+			BASE="${GREEN}INFO${NC} "
+			;;
+		-w )
+			BASE="${BLUE}WARN${NC} "
+			;;
+		-e )
+			BASE="${RED}ERROR${NC} "
+			;;
+	esac
+	echo -e "${BASE}${message}"
+}
+
 function config() {
-	echo "[INFO] Writting to .bash_profile..."
+	log -i "Writting to .bash_profile..."
 	SOURCE_SCRIPT_LINE="source $HOME/code/scripts/palanrc.sh"
 	if ! grep -x -q "$SOURCE_SCRIPT_LINE" ~/.bash_profile ; then
 		echo $SOURCE_SCRIPT_LINE >> ~/.bash_profile
 	fi
 	source ~/.bash_profile
-	echo "[INFO] Writting to .bash_profile complete."
+	log -i "Writting to .bash_profile complete."
 
-	echo "[INFO] Copying gitconfig file to '$HOME'..."
+	log -i "Copying gitconfig file to '$HOME'..."
 	if [[ ! -f $HOME/.gitconfig ]]; then
-		cp $HOME/code/scripts/gitconfig $HOME/.gitconfig && echo "[INFO] Copying gitconfig file to '$HOME' complete."
+		cp $HOME/code/scripts/gitconfig $HOME/.gitconfig && log -i "Copying gitconfig file to '$HOME' complete."
 	else
-		echo "[WARN] Aborted: gitconfig file already exists."
+		log -w "Aborted: gitconfig file already exists."
 	fi
 
 }
 
 function install() {
-	echo "[INFO] Installing brew..."
+	log -i "Installing brew..."
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	echo "[INFO] Installing brew complete."
+	log -i "Installing brew complete."
 
-	echo "[INFO] Installing GNU parallel..."
+	log -i "Installing GNU parallel..."
 	brew install parallel
-	echo "[INFO] Installing GNU parallel complete."
+	log -i "Installing GNU parallel complete."
 
-	echo "[INFO] Installing brew programs..."
-	echo "[INFO] This will take a while and you wont see any download progress."
+	log -i "Installing brew programs..."
+	log -i "This will take a while and you wont see any download progress."
 	parallel brew fetch ::: elasticsearch kibana node yarn mysql thefuck ctop sbt unrar httpie git bash-completion hub
 	brew install elasticsearch kibana node yarn mysql thefuck ctop sbt unrar httpie git bash-completion hub
 	brew upgrade
-	echo "[INFO] Installing brew programs complete."
+	log -i "Installing brew programs complete."
 
-	echo "[INFO] Installing brew cask programs..."
-	echo "[INFO] This will take a while and you wont see any download progress."
+	log -i "Installing brew cask programs..."
+	log -i "This will take a while and you wont see any download progress."
 	parallel brew cask fetch ::: google-chrome spotify whatsapp java8 intellij-idea docker-edge datagrip webstorm slack font-fira-code spectacle steam sublime-text tomighty qbittorrent spotify-notifications vlc
 	brew cask install google-chrome spotify whatsapp java8 intellij-idea docker-edge datagrip webstorm slack font-fira-code spectacle steam sublime-text tomighty qbittorrent spotify-notifications vlc
 	brew cask upgrade
-	echo "[INFO] Installing brew cask programs complete."
+	log -i "Installing brew cask programs complete."
 }
 
 function fetch() {
-	cd $PALAN_SCRIPTS && git pull && echo "[INFO] Repo succesfully updated" || echo "[ERROR] Couldn't update repo"
+	cd $PALAN_SCRIPTS && git pull && log -i "Repo succesfully updated" || log -e "Couldn't update repo"
 	cd - > /dev/null
 }
 
 function cleanup() {
-	"[INFO] Erasing brew and brew cask cache..."
+	log -i "Erasing brew and brew cask cache..."
 	rm -rf $(brew --cache)
 	brew cask cleanup
-	"[INFO] Erasing brew and brew cask cache complete."
+	log -i "Erasing brew and brew cask cache complete."
 }
 
 function update() {
-	echo "not implemented yet."
+	log -e "Not implemented yet."
 }
 
 if [[ $1 == "install" ]]; then
@@ -73,5 +98,5 @@ if [[ $1 == "cleanup" ]]; then
 fi
 
 if [[ $1 == "" ]]; then
-	echo "usage: . install.sh {install|config|fetch|update|cleanup}"
+	echo "Usage: . install.sh { install | config | fetch | update | cleanup }"
 fi
