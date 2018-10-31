c4b7f8cd2e623c7de440d68e4a7a01b40049b3de
palanga <a.gonzalez.terres@gmail.com>
2018-10-30 22:50:18 -0300

    Make repo status parsing output language agnostic

--- palanrc.sh
+++ palanrc.sh
@@ -83,7 +83,7 @@ eval $(thefuck --alias)
 
 # get current branch in git repo
 function parse_git_branch() {
-	BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+	BRANCH=`git rev-parse --abbrev-ref HEAD 2> /dev/null`
 	if [ ! "${BRANCH}" == "" ]
 	then
 		STAT=`parse_git_dirty`
@@ -95,31 +95,33 @@ function parse_git_branch() {
 
 # get current status of git repo
 function parse_git_dirty {
-	status=`git status 2>&1 | tee`
-	dirty=`echo -n "${status}" 2> /dev/null | grep "modified:" &> /dev/null; echo "$?"`
-	untracked=`echo -n "${status}" 2> /dev/null | grep "Untracked files" &> /dev/null; echo "$?"`
-	ahead=`echo -n "${status}" 2> /dev/null | grep "Your branch is ahead of" &> /dev/null; echo "$?"`
-	newfile=`echo -n "${status}" 2> /dev/null | grep "new file:" &> /dev/null; echo "$?"`
-	renamed=`echo -n "${status}" 2> /dev/null | grep "renamed:" &> /dev/null; echo "$?"`
-	deleted=`echo -n "${status}" 2> /dev/null | grep "deleted:" &> /dev/null; echo "$?"`
+	status=$(git status --porcelain | awk '{print $1}')
+
+	modified=`echo $status | grep "M" -q; echo $?`
+	untracked=`echo $status | grep "??" -q; echo $?`
+	newfile=`echo $status | grep "A" -q; echo $?`
+	renamed=`echo $status | grep "R" -q; echo $?`
+	deleted=`echo $status | grep "D" -q; echo $?`
+
+	# TODO find a way to parse despite of the output language
+	# ahead=`echo -n "${status}" 2> /dev/null | grep "Your branch is ahead of" &> /dev/null; echo "$?"`
+
 	bits=''
-	if [ "${renamed}" == "0" ]; then
-		bits=">${bits}"
+
+	if [ $modified == 0 ]; then
+		bits="!${bits}"
 	fi
-	if [ "${ahead}" == "0" ]; then
-		bits="*${bits}"
-	fi
-	if [ "${newfile}" == "0" ]; then
-		bits="+${bits}"
-	fi
-	if [ "${untracked}" == "0" ]; then
+	if [ $untracked == 0 ]; then
 		bits="?${bits}"
 	fi
-	if [ "${deleted}" == "0" ]; then
-		bits="x${bits}"
+	if [ $newfile == 0 ]; then
+		bits="+${bits}"
 	fi
-	if [ "${dirty}" == "0" ]; then
-		bits="!${bits}"
+	if [ $renamed == 0 ]; then
+		bits=">${bits}"
+	fi
+	if [ $deleted == 0 ]; then
+		bits="x${bits}"
 	fi
 	if [ ! "${bits}" == "" ]; then
 		echo " ${bits}"
